93c904a785b9d8e6b6332961059938ec81f6cb05
juliannordang <julian@getmail.no>
2026-02-19 13:44:55 +0100

    a

--- lab_2-1.1.4/skeleton_project/source/logic.h
+++ lab_2-1.1.4/skeleton_project/source/logic.h
@@ -1,19 +1,31 @@
-#include "logic.c"
+#include <stdbool.h>
+#include "sensors.h"
+typedef struct{
+    //variabler
+    int voltage;
+    bool directionUp;
 
+}Elevator;
+
+//liksomMetoder
+void elevatorChange(Elevator* e,int newVoltage, bool newDirectionUp);
 
 typedef struct {
     //variabler
     Elevator elevator;
     Etasjepanel etasjepanel;
     Heispanel heispanel;
-    Sensor[4] sensors;
-    int[6] queue;
+    Sensor sensors[4];
+    int queue[6];
     Button obstructionButton;
     int story;
 
-    //metoder
-    void updateQueue();
-    void updateStory();
-    void updateAllSensors();
-    void run();
 }QueueManager;
+//liksomMetoder
+void updateQueue(QueueManager* q);
+void updateStory(QueueManager* q);
+void updateAllSensors(QueueManager* q);
+void updateEverything(QueueManager* q);
+void run(QueueManager* q);
+
+
