7c8c330a21e618b58ff13b18b4753265f1f3bbef
JAKAP42 <117973377+JAKAP42@users.noreply.github.com>
2026-02-19 14:10:42 +0100

    added functions

--- lab_2-1.1.4/skeleton_project/source/sensors.c
+++ lab_2-1.1.4/skeleton_project/source/sensors.c
@@ -1,16 +1,10 @@
 #include "sensors.h"
-#include "driver/elevio.h"
 
-void callUpdate(CallButton* call) {
-    if (call == NULL) {
-        return;
-    }
-
-    if (call->story < 0 || call->story >= N_FLOORS) {
+updateButton(CallButton* call){
+    int active = elevio_callButton(call->buttonType);
+    if (active == 1){
+        call->active = true;
+    } else {
         call->active = false;
-        return;
     }
-
-    ButtonType button = call->directionUp ? BUTTON_HALL_UP : BUTTON_HALL_DOWN;
-    call->active = elevio_callButton(call->story, button) != 0;
 }
